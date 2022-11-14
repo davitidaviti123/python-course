d5f097d97528192787fe2b4b66fa4dadd69f2795
davitidaviti123 <davit.meparishvili2011@gmail.com>
2022-11-14 21:22:49 +0400

    first commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,11 @@
+age = 11 #int integer
+name = "daviti"
+surname = "meparishvili" 
+
+print("chemi saxelia " + name +  " chemi gvaria "  +  surname +  " me var "  +  str(age)  +  " wlis")
+
+year = 5
+age += year
+
+#გავიდა 5 წელი და გავხდი 16 წლის
+print("gavida " + str(year) + " weli" + " da gavxdi " + str(age) + " wlis")
---
+++ day 2/lesson2.py
@@ -0,0 +1,21 @@
+name = "daviti"
+
+print(len(name))   #len lenght
+          #6
+my_sentence = """გამარჯობა, შეგახსენებთ რომ სწავლ ას იწყებთ 19 სექტემბერს, გთხოვთ არ გააცდინოთ"""
+
+# print(my_sentence)
+
+#knows programing = True
+
+# if 10<5 :
+# print("hello")
+#tab-ის საშუალებით მოვახდინეტ ინდენტაცია
+
+full_name = "davit meparishvili"
+# print(len(full_name))
+
+# print(full_name[7])
+#position = index სინონიმებია
+
+print("k" not in full_name)
